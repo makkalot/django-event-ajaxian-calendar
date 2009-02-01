b71929c589f223c61f0c05fbf28b3f72a203b109
user <user@1f296ece-db5a-0410-9122-ccecd2622b0e>
2009-02-01 19:47:45 +0000

    tiny added to missing places
    git-svn-id: svn://freebsd.kuwaitnet.net/loadair/trunk/school/schoolcalendar@247 1f296ece-db5a-0410-9122-ccecd2622b0e

--- admin.py
+++ admin.py
@@ -6,13 +6,24 @@ class SchoolCalendarCategoryAdmin(admin.ModelAdmin):
     list_display = ('english_name','arabic_name')
     list_display_links = ('english_name',)
     ordering = ['english_name',]
+    
+    class Media:
+        js = (
+                '/static/Script__en/tinymce/jscripts/tiny_mce/tiny_mce.js',
+                '/static/Script__en/tinymce/textareas.js',
+        )
 
 class SchoolCalendarEventAdmin(admin.ModelAdmin):
     list_display = ('english_title','arabic_title', 'category','start_date')
     ordering = ['category','-start_date']
     list_filter = ('start_date','category',)
     search_fields = ['english_title','arabic_title']
-
+    
+    class Media:
+        js = (
+                '/static/Script__en/tinymce/jscripts/tiny_mce/tiny_mce.js',
+                '/static/Script__en/tinymce/textareas.js',
+        )
 
 admin.site.register(SchoolCalendarCategory,SchoolCalendarCategoryAdmin)
 admin.site.register(SchoolCalendarEvent, SchoolCalendarEventAdmin)
